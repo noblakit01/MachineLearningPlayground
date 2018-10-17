b8c320ae5a02b71611bd7275e26a5f0b835a3ff8
Luan Tran <luan.tran@mowede.com>
2018-10-17 14:43:49 +0700

    Add example  detect object with CoreML

---
+++ ObjectDetectionWithCoreML/ObjectDetectionWithCoreML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectDetectionWithCoreML
+//
+//  Created by luantran on 10/17/18.
+//  Copyright © 2018 mowede. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
