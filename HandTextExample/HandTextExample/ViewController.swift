4b17c37ed06cba80058b7c392710ca076f3f0cc7
Luan Tran <luan.tran@mowede.com>
2018-10-22 09:44:45 +0700

    Implement MNIST model and test hand drawing detect number

--- HandTextExample/HandTextExample/ViewController.swift
+++ HandTextExample/HandTextExample/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
     
@@ -14,6 +16,20 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var resetButton: UIBarButtonItem!
     @IBOutlet weak var detectButton: UIBarButtonItem!
+    @IBOutlet weak var classificationLabel: UILabel!
+    
+    lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            let model = try VNCoreMLModel(for: MNIST().model)
+            let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
+                self?.processClassifications(for: request, error: error)
+            })
+            request.imageCropAndScaleOption = .scaleFit
+            return request
+        } catch {
+            fatalError("Failed to load Vision ML model: \(error)")
+        }
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +47,48 @@ class ViewController: UIViewController {
     }
     
     @IBAction func detect() {
+        guard let image = signatureViewController.fullSignatureImage else {
+            return
+        }
+        updateClassifications(for: image)
+    }
+    
+    func updateClassifications(for image: UIImage) {
+        classificationLabel.text = "Classifying..."
         
+        let orientation = CGImagePropertyOrientation(image.imageOrientation)
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("Unable to create \(CIImage.self) from \(image).")
+        }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let handler = VNImageRequestHandler(ciImage: ciImage)
+            do {
+                try handler.perform([self.classificationRequest])
+            } catch {
+                print("Failed to perform classification.\n\(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func processClassifications(for request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            guard let results = request.results else {
+                self.classificationLabel.text = "Unable to classify image.\n\(error!.localizedDescription)"
+                return
+            }
+            let classifications = results as! [VNClassificationObservation]
+            
+            if classifications.isEmpty {
+                self.classificationLabel.text = "Nothing recognized."
+            } else {
+                let topClassifications = classifications.prefix(2)
+                let descriptions = topClassifications.map { classification in
+                    return String(format: "  (%.2f) %@", classification.confidence, classification.identifier)
+                }
+                self.classificationLabel.text = "Classification:\n" + descriptions.joined(separator: "\n")
+            }
+        }
     }
 }
 
